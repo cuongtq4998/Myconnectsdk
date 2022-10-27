9856bd292feee953b544dcb4be1e60dc99b88f59
tranquoccuong <cuongtq4998@gmail.com>
2022-10-27 21:48:37 +0700

    init source

---
+++ MyConnectSDK/MyConnectSDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyConnectSDK
+//
+//  Created by Quoc Cuong on 27/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyConnectSDK/MyConnectSDKUITests/MyConnectSDKUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyConnectSDKUITestsLaunchTests.swift
+//  MyConnectSDKUITests
+//
+//  Created by Quoc Cuong on 27/10/2022.
+//
+
+import XCTest
+
+final class MyConnectSDKUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
